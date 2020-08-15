bd2a626bf3a38f38038827235e164446b77a5d11
vhavlena <vohavlena@gmail.com>
2020-08-15 18:08:18 +0200

    Hard cases detection

---
+++ src/units/test-sch-hard.cpp
@@ -0,0 +1,73 @@
+
+#include <iostream>
+#include <set>
+#include <map>
+#include <fstream>
+#include <algorithm>
+
+#include "units-config.h"
+#include "../BuchiAutomaton.h"
+#include "../Simulations.h"
+#include "../BuchiAutomatonSpec.h"
+#include "../BuchiAutomataParser.h"
+#include "../AuxFunctions.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  BuchiAutomataParser parser;
+  ifstream os;
+
+  if(argc != 2)
+  {
+    cerr << "Bad arguments" << endl;
+    return 1;
+  }
+  string filename(argv[1]);
+  os.open(argv[1]);
+
+  if(os)
+  {
+    BuchiAutomaton<string, string> ba = parser.parseBaFormat(os);
+    string cmd = "java -jar " + RABITEXE + " " + filename + " " + filename + " -dirsim";
+    Simulations sim;
+    istringstream strr(Simulations::execCmd(cmd));
+    ba.setDirectSim(sim.parseRabitRelation(strr));
+    auto cl = set<std::string>();
+    ba.computeRankSim(cl);
+
+    BuchiAutomaton<int, int> ren = ba.renameAut();
+    BuchiAutomatonSpec sp(ren);
+    BuchiAutomaton<StateSch, int> comp = sp.complementSchNFA(sp.getInitials());
+    set<StateSch> slIgnore = sp.nfaSlAccept(comp);
+
+    auto maxReach = sp.getMaxReachSize(comp, slIgnore);
+    auto minReach = sp.getMinReachSize();
+    auto maxReachInd = sp.getMaxReachSizeInd();
+
+    auto slnoAccept = sp.nfaSingleSlNoAccept(comp);
+    set<StateSch> ignoreAll;
+    for(const auto& t : slnoAccept)
+      ignoreAll.insert({t.first, set<int>(), RankFunc(), 0, false});
+    ignoreAll.insert(slIgnore.begin(), slIgnore.end());
+
+    auto rankBound = sp.getRankBound(comp, ignoreAll, maxReach, minReach);
+
+    bool suit = true;
+    for(auto t : comp.getCycleClosingStates(slIgnore))
+    {
+      if((t.S.size() >= 9 && rankBound[t.S] >= 5) || (t.S.size() >= 8 && rankBound[t.S] >= 6))
+      {
+        suit = false;
+        break;
+      }
+    }
+
+    cout << std::boolalpha;
+    cout << suit << endl;
+
+  }
+  os.close();
+  return 0;
+}
