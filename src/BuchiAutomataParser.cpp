5fc5e673f70c7a97b63ef7bb4feb7c98eb9af674
vhavlena <vohavlena@gmail.com>
2020-09-02 13:53:32 +0200

    parser: gff format

--- src/BuchiAutomataParser.cpp
+++ src/BuchiAutomataParser.cpp
@@ -77,3 +77,87 @@ Transition<string, string> BuchiAutomataParser::parseBATransition(string line)
   }
   return tr;
 }
+
+
+BuchiAutomaton<string, string> BuchiAutomataParser::parseGffFormat(string& str)
+{
+  boost::algorithm::to_lower(str);
+  istringstream iss (str);
+
+  pt::ptree tree;
+  pt::read_xml(iss, tree);
+  return parseGffTree(tree);
+}
+
+
+BuchiAutomaton<string, string> BuchiAutomataParser::parseGffFormat(ifstream& is)
+{
+  std::string str((std::istreambuf_iterator<char>(is)),
+                 std::istreambuf_iterator<char>());
+  return parseGffFormat(str);
+}
+
+
+BuchiAutomaton<string, string> BuchiAutomataParser::parseGffTree(pt::ptree& tree)
+{
+  set<string> alph;
+  for(pt::ptree::value_type &v : tree.get_child("structure.alphabet"))
+  {
+    if(v.first == "symbol")
+      alph.insert(v.second.data());
+  }
+
+  set<string> states;
+  for(pt::ptree::value_type &v : tree.get_child("structure.stateset"))
+  {
+    states.insert(v.second.get_child("<xmlattr>.sid").data());
+  }
+
+  set<string> fin;
+  for(pt::ptree::value_type &v : tree.get_child("structure.acc"))
+  {
+    if(v.first == "stateid")
+      fin.insert(v.second.data());
+  }
+
+  set<string> ini;
+  for(pt::ptree::value_type &v : tree.get_child("structure.initialstateset"))
+  {
+    if(v.first == "stateid")
+      ini.insert(v.second.data());
+  }
+
+  BuchiAutomaton<string, string>::Transitions trans;
+  for(pt::ptree::value_type &v : tree.get_child("structure.transitionset"))
+  {
+    if(v.first != "transition")
+      continue;
+    Transition<string, string> tr = parseGffTransition(v.second);
+    pair<string, string> pr = make_pair(tr.from, tr.symbol);
+    if(trans.find(pr) == trans.end())
+      trans.insert({pr, set<string>({tr.to})});
+    else
+      trans[pr].insert(tr.to);
+  }
+
+  return BuchiAutomaton<string, string>(states, fin, ini, trans, alph);
+}
+
+
+Transition<string, string> BuchiAutomataParser::parseGffTransition(pt::ptree& tr)
+{
+  Transition<string, string> ret;
+  ret.from = tr.get_child("from").data();
+  ret.to = tr.get_child("to").data();
+
+  boost::optional< pt::ptree& > sym = tr.get_child_optional("label");
+  if(!sym)
+  {
+    ret.symbol = tr.get_child("read").data();
+  }
+  else
+  {
+    ret.symbol = sym->data();
+  }
+  return ret;
+}
--- src/BuchiAutomataParser.h
+++ src/BuchiAutomataParser.h
@@ -4,18 +4,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <streambuf>
 #include <boost/algorithm/string.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/optional/optional.hpp>
 #include "BuchiAutomaton.h"
 
 using namespace std;
+namespace pt = boost::property_tree;
 
 class BuchiAutomataParser {
 
 public:
-  BuchiAutomaton<std::string, std::string> parseBaFormat(ifstream & os);
+  BuchiAutomaton<string, string> parseBaFormat(ifstream & os);
+  BuchiAutomaton<string, string> parseGffFormat(string& str);
+  BuchiAutomaton<string, string> parseGffFormat(ifstream& is);
 
 private:
   Transition<string, string> parseBATransition(string line);
+  Transition<string, string> parseGffTransition(pt::ptree& tr);
+  BuchiAutomaton<string, string> parseGffTree(pt::ptree& tr);
 
 };
 
