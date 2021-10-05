b5433e18e777eab81f123318cd3edd102172bf82
vhavlena <vohavlena@gmail.com>
2021-10-05 09:41:40 +0200

    light: settings

--- src/ranker.cpp
+++ src/ranker.cpp
@@ -191,7 +191,7 @@ int main(int argc, char *argv[])
     opt.reach = false;
     opt.succEmptyCheck = false;
     opt.elevator.elevatorRank = true;
-    opt.dataFlow = LIGHT;
+    //opt.dataFlow = LIGHT;
   }
 
 
