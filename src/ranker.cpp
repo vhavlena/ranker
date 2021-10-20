fff98de5dbc926781e4fe93b91a9045b33f9712f
vhavlena <vohavlena@gmail.com>
2021-10-20 08:44:33 +0200

    ranker: settings

--- src/ranker.cpp
+++ src/ranker.cpp
@@ -48,8 +48,8 @@ int main(int argc, char *argv[])
   args::Flag accPropagationFlag(parser, "acc-propagation", "Propagate accepting states in each SCC", {"acc-propagation"});
   args::Flag sdFlag(parser, "sd", "Use semideterminization", {"sd"});
 
-  ComplOptions opt = { .cutPoint = true, .succEmptyCheck = false, .ROMinState = 7,
-      .ROMinRank = 7, .CacheMaxState = 6, .CacheMaxRank = 8, .semidetOpt = false,
+  ComplOptions opt = { .cutPoint = true, .succEmptyCheck = false, .ROMinState = 8,
+      .ROMinRank = 6, .CacheMaxState = 6, .CacheMaxRank = 8, .semidetOpt = false,
       .dataFlow = INNER, .delay = false, .delayVersion = oldVersion, .delayW = 0.5,
       .debug = false, .elevator = { .elevatorRank = true, .detBeginning = false },
       .sim = true, .sl = true, .reach = true, .flowDirSim = false, .preprocess = NONE, .accPropagation = false,
