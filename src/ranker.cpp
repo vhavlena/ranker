77812664f5df8b011c3121a29faa6f791438902b
vhavlena <vohavlena@gmail.com>
2021-10-27 15:46:36 +0200

    ranker: minor

--- src/ranker.cpp
+++ src/ranker.cpp
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
       .ROMinRank = 6, .CacheMaxState = 6, .CacheMaxRank = 8, .semidetOpt = false,
       .dataFlow = INNER, .delay = false, .delayVersion = oldVersion, .delayW = 0.5,
       .debug = false, .elevator = { .elevatorRank = true, .detBeginning = false },
-      .sim = true, .sl = true, .reach = true, .flowDirSim = false, .preprocess = NONE, .accPropagation = true,
+      .sim = true, .sl = true, .reach = true, .flowDirSim = false, .preprocess = NONE, .accPropagation = false,
       .semideterminize = false, .backoff = false, .BOBound = { {11,15} },
       .semideterministic = false, .complete = false };
 
